f7ab49c827f838520f587a5bb5f11ef0f5bac770
ODEit <jose.sabal@yahoo.com>
2018-05-04 09:24:50 -0400

    commented out a part from post part

--- server/api/dicomInfo.js
+++ server/api/dicomInfo.js
@@ -25,36 +25,36 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
     if (req.user) {
-        // res.json({
-        //     studyDesc: 'hi',
-        // })
-
-        const genKey = nano()
-        let image = new Buffer(req.body.image.replace(/^data:image\/\w+;base64,/, ""), 'base64')
-        const params = {
-            Bucket: 'dicom-trial',
-            Key: genKey,
-            Body: image,
-            ContentEncoding: 'base64',
-            ContentType: 'image/jpeg',
-            ACL: 'public-read' //permissions
-        }
-        req.body.imageURL = `https://s3.amazonaws.com/dicom-trial/${genKey}`
-        s3.putObject(params, (err, data) => {
-            if (err) {
-                console.log(err);
-                res.status(400).send(err.message)
-            } else {
-                DicomInfo.create(req.body)
-                    .then(dicomInfo => {
-                        req.user.addDicom(dicomInfo)
-                        res.json(dicomInfo)
-                    })
-            }
+        res.json({
+            studyDesc: 'hi',
         })
-    }
-    else {
-        res.send("Unauthorized")
+
+    //     const genKey = nano()
+    //     let image = new Buffer(req.body.image.replace(/^data:image\/\w+;base64,/, ""), 'base64')
+    //     const params = {
+    //         Bucket: 'dicom-trial',
+    //         Key: genKey,
+    //         Body: image,
+    //         ContentEncoding: 'base64',
+    //         ContentType: 'image/jpeg',
+    //         ACL: 'public-read' //permissions
+    //     }
+    //     req.body.imageURL = `https://s3.amazonaws.com/dicom-trial/${genKey}`
+    //     s3.putObject(params, (err, data) => {
+    //         if (err) {
+    //             console.log(err);
+    //             res.status(400).send(err.message)
+    //         } else {
+    //             DicomInfo.create(req.body)
+    //                 .then(dicomInfo => {
+    //                     req.user.addDicom(dicomInfo)
+    //                     res.json(dicomInfo)
+    //                 })
+    //         }
+    //     })
+    // }
+    // else {
+    //     res.send("Unauthorized")
     }
 })
 
