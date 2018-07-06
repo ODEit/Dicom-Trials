a0e67121696d1a776cc47577721ef7ede6608e48
ODEit <jose.sabal@yahoo.com>
2018-07-06 08:27:29 -0400

    uncommented dicom part

--- server/api/dicomInfo.js
+++ server/api/dicomInfo.js
@@ -12,49 +12,49 @@ AWS.config.update({
     subregion: 'us-east-1',
 });
 
-router.get('/', (req, res, next) => {    
+router.get('/', (req, res, next) => {
     if (req.user) {
         req.user.getDicom()
             .then(dicoms => {
                 res.json(dicoms)
             })
-        }else {
-            res.send('unauthorized')
-        }
+    } else {
+        res.send('unauthorized')
+    }
 })
 
 router.post('/', (req, res, next) => {
     if (req.user) {
-        res.json({
-            studyDesc: 'hi',
-        })
+        // res.json({
+        //     studyDesc: 'hi',
+        // })
 
-    //     const genKey = nano()
-    //     let image = new Buffer(req.body.image.replace(/^data:image\/\w+;base64,/, ""), 'base64')
-    //     const params = {
-    //         Bucket: 'dicom-trial',
-    //         Key: genKey,
-    //         Body: image,
-    //         ContentEncoding: 'base64',
-    //         ContentType: 'image/jpeg',
-    //         ACL: 'public-read' //permissions
-    //     }
-    //     req.body.imageURL = `https://s3.amazonaws.com/dicom-trial/${genKey}`
-    //     s3.putObject(params, (err, data) => {
-    //         if (err) {
-    //             console.log(err);
-    //             res.status(400).send(err.message)
-    //         } else {
-    //             DicomInfo.create(req.body)
-    //                 .then(dicomInfo => {
-    //                     req.user.addDicom(dicomInfo)
-    //                     res.json(dicomInfo)
-    //                 })
-    //         }
-    //     })
-    // }
-    // else {
-    //     res.send("Unauthorized")
+        const genKey = nano()
+        let image = new Buffer(req.body.image.replace(/^data:image\/\w+;base64,/, ""), 'base64')
+        const params = {
+            Bucket: 'dicom-trial',
+            Key: genKey,
+            Body: image,
+            ContentEncoding: 'base64',
+            ContentType: 'image/jpeg',
+            ACL: 'public-read' //permissions
+        }
+        req.body.imageURL = `https://s3.amazonaws.com/dicom-trial/${genKey}`
+        s3.putObject(params, (err, data) => {
+            if (err) {
+                console.log(err);
+                res.status(400).send(err.message)
+            } else {
+                DicomInfo.create(req.body)
+                    .then(dicomInfo => {
+                        req.user.addDicom(dicomInfo)
+                        res.json(dicomInfo)
+                    })
+            }
+        })
+    }
+    else {
+        res.send("Unauthorized")
     }
 })
 
